5107c192ebd9f8fb1b66a781d0b6a841105d570e
Nakanishi Toshihiko <binarhythm_bb@yahoo.co.jp>
2021-02-07 16:40:49 +0900

    2021/02/07 16:40

--- MemoPad/ViewController.swift
+++ MemoPad/ViewController.swift
@@ -6,14 +6,31 @@
 //
 
 import UIKit
-
+import SpriteKit
 class ViewController: UIViewController {
-
+    
+    var skView: SKView?
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        createSKView() // SKView作成
+        setupParticle() // Sceneを追加
     }
-
-
+    
+    func createSKView() {
+        self.skView = SKView(frame: self.view.frame)
+        self.skView!.allowsTransparency = true
+    }
+    
+    func setupParticle() {
+        let scene = SKScene(size: self.view.frame.size)
+        //scene.backgroundColor = UIColor.clear
+        let path = Bundle.main.path(forResource: "MyParticle", ofType: "sks")!
+        let data = try! Data(contentsOf: URL(fileURLWithPath: path))
+        let particle = try! NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(data) as! SKEmitterNode
+        particle.name = "MyParticle"
+        particle.position = CGPoint(x: self.view.frame.width / 2, y: self.view.frame.height / 2)
+        scene.addChild(particle)
+        self.skView!.presentScene(scene)
+        self.view.addSubview(self.skView!)
+        }
 }
-
